a39ea208deb5f8b6a6d9a3432910d498c8b06fac
fangyuandoit <676176407@qq.com>
2020-08-05 14:57:35 +0800

    zookeeper main test

---
+++ springboot_zk_dubbo/src/main/java/com/fy/springboot_zk_dubbo/SpringbootZkDubboApplication.java
@@ -0,0 +1,13 @@
+package com.fy.springboot_zk_dubbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootZkDubboApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootZkDubboApplication.class, args);
+    }
+
+}
